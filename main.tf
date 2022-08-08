8d5e0e1db827c1133a70324c526656fe2093b394
Red Cricket <red.cricket.blog@gmail.com>
2022-08-07 18:08:37 -0700

    adsfa

--- main.tf
+++ main.tf
@@ -1,3 +1,17 @@
+terraform {
+  required_providers {
+    # We recommend pinning to the specific version of the Docker Provider you're using
+    # since new versions are released frequently
+    docker = {
+      source  = "kreuzwerker/docker"
+      version = "2.13.0"
+    }
+  }
+}
+
+# Configure the docker provider
+provider "docker" {
+}
 # Or create a service resource
 # -> same as 'docker service create -d -p 8081:80 --name nginx-service --replicas 2 nginx:latest'
 resource "docker_service" "nginx_service" {
